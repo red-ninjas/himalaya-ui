0d99d3d2ffcf7ded2bad897e3ade92aca1b89898
muhammad.jahanzeb <muhammad.jahanzeb@edetek.com>
2023-10-30 19:37:21 +0500

    refined css for menu item component

--- src/components/menu/menu-item.tsx
+++ src/components/menu/menu-item.tsx
@@ -1,12 +1,11 @@
 import { PropsWithChildren } from 'react';
+import useScale, { withScale } from '../use-scale';
 import useTheme from '../use-theme';
 import { MenuItemProps } from './index';
 
-export default function MenuItem({
-  children,
-  ...other
-}: PropsWithChildren<MenuItemProps>) {
+function MenuItemComponent({ children, ...other }: PropsWithChildren<MenuItemProps>) {
   const theme = useTheme();
+  const { SCALES } = useScale();
   return (
     <>
       <div {...other} className="menu-item">
@@ -17,6 +16,9 @@ export default function MenuItem({
           display: flex;
           justify-content: flex-start;
           align-items: center;
+          font-size: ${SCALES.font(0.9375)};
+          padding-top: ${SCALES.font(0.1875)};
+          padding-bottom: ${SCALES.font(0.1875)};
 
           &:hover {
             background: ${theme.expressiveness.linkHoverStyle};
@@ -26,3 +28,8 @@ export default function MenuItem({
     </>
   );
 }
+
+MenuItemComponent.displayName = 'HimalayaMenuItem';
+const MenuItem = withScale(MenuItemComponent);
+
+export default MenuItem;
