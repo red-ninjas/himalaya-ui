0ca8e1ba8248b4b602734d951c9f445877bfc635
Syed Abubakar <developer@awbnext.com>
2023-11-02 11:18:41 +0500

    update collect-meta to add desription

--- scripts/collect-meta.js
+++ scripts/collect-meta.js
@@ -1,11 +1,11 @@
-const fs = require('fs-extra')
-const path = require('path')
-const extractMetadata = require('extract-mdx-metadata')
-const pagePrefix = path.join(__dirname, '../src/app')
+const fs = require('fs-extra');
+const path = require('path');
+const extractMetadata = require('extract-mdx-metadata');
+const pagePrefix = path.join(__dirname, '../src/app');
 
 const getTargetPath = () => {
-  return path.join(__dirname, '../src/lib/data/', `metadata.json`)
-}
+  return path.join(__dirname, '../src/lib/data/', `metadata.json`);
+};
 
 const weights = {
   guide: 1,
@@ -13,7 +13,7 @@ const weights = {
   'getting-started': 3,
   components: 5,
   customization: 10,
-}
+};
 const groupWeights = {
   general: 1,
   layout: 2,
@@ -25,99 +25,120 @@ const groupWeights = {
   others: 8,
   utils: 10,
   effects: 12,
-}
+};
+
+const getDescription = content => {
+  const lines = content.split('\n');
+  let description = '';
+
+  const headingIndex = lines.findIndex(line => line.startsWith('# '));
+  let stopExtraction = false;
+
+  if (headingIndex !== -1) {
+    for (let i = headingIndex + 1; i < lines.length; i++) {
+      const line = lines[i].trim();
+      if (line.startsWith('<Playground')) {
+        stopExtraction = true;
+      }
+
+      if (stopExtraction || line.startsWith('# ') || line.startsWith('```')) {
+        break;
+      }
+
+      if (!line.startsWith('## ') && line !== '') {
+        description += line + ' ';
+      }
+    }
+  }
+  return description.trim();
+};
 
 const getMetadata = async (files, parentPath) => {
   return Promise.all(
     files
       .filter(name => name.endsWith('.mdx') || !name.includes('.'))
       .map(async file => {
-        const filePath = path.join(parentPath, file)
+        const filePath = path.join(parentPath, file);
 
-        const isDirectory = fs.statSync(filePath).isDirectory()
+        const isDirectory = fs.statSync(filePath).isDirectory();
         if (isDirectory) {
-          let hasPage = false
+          let hasPage = false;
           try {
-            hasPage = fs.statSync(path.join(filePath, 'page.tsx'))
+            hasPage = fs.statSync(path.join(filePath, 'page.tsx'));
           } catch (err) {
-            hasPage = false
+            hasPage = false;
           }
 
           if (!hasPage) {
-            const children = await fs.readdir(filePath)
-            const childrenMetadata = await getMetadata(children, filePath)
-            const sorted = childrenMetadata.sort((a, b) => a.index - b.index)
+            const children = await fs.readdir(filePath);
+            const childrenMetadata = await getMetadata(children, filePath);
+            const sorted = childrenMetadata.sort((a, b) => a.index - b.index);
 
             // grouping
             const childrenHasGroup = sorted
               .filter(item => !!item)
               .find(item => {
-                return item?.group ?? {}
-              })
+                return item?.group ?? {};
+              });
             if (childrenHasGroup) {
-              const groups = [...new Set(sorted.map(item => item.group || 'others'))]
+              const groups = [...new Set(sorted.map(item => item.group || 'others'))];
               const groupChildren = groups
                 .map(groupName => ({
                   name: groupName,
                   children: sorted.filter(item => (item.group || 'others') === groupName),
                 }))
                 .sort((a, b) => {
-                  const pre = a.name.toLowerCase()
-                  const current = b.name.toLowerCase()
-                  return groupWeights[pre] - groupWeights[current]
-                })
+                  const pre = a.name.toLowerCase();
+                  const current = b.name.toLowerCase();
+                  return groupWeights[pre] - groupWeights[current];
+                });
               return {
                 name: file,
                 url: `/${file}`,
                 children: groupChildren,
-              }
+              };
             }
 
-            return { name: file, children: sorted }
+            return { name: file, children: sorted };
           } else {
-            const previousFolder = path.basename(filePath)
+            const previousFolder = path.basename(filePath);
             try {
-              const content = await fs.readFile(
-                path.join(filePath, previousFolder + '.mdx'),
-                'utf-8',
-              )
+              const content = await fs.readFile(path.join(filePath, previousFolder + '.mdx'), 'utf-8');
 
-              const meta = await extractMetadata(content)
-              const url = filePath
-                .replace(pagePrefix, '')
-                .replace('.mdx', '')
-                .replace(/\\/g, '/')
-                .toString()
+              const meta = await extractMetadata(content);
+
+              const description = getDescription(content);
+
+              const url = filePath.replace(pagePrefix, '').replace('.mdx', '').replace(/\\/g, '/').toString();
               const val = {
                 name: meta.title || file,
                 url: url,
                 index: meta.index || 100,
                 group: meta.group || null,
-              }
-              return val
+                description: description,
+              };
+              return val;
             } catch (e) {
-              console.error('error in', path.join(filePath, previousFolder + '.mdx'))
-              throw e
+              console.error('error in', path.join(filePath, previousFolder + '.mdx'));
+              throw e;
             }
           }
         }
       }),
-  )
-}
+  );
+};
 
-  ; (async () => {
-    try {
-      const dirs = await fs.readdir(pagePrefix)
-      const childDirs = dirs.filter(df =>
-        fs.statSync(path.join(pagePrefix, df)).isDirectory(),
-      )
-      const data = await getMetadata(childDirs, pagePrefix)
-      const sorted = data.sort((a, b) => weights[a.name] - weights[b.name])
-      const targetPath = getTargetPath()
-      await fs.ensureFile(targetPath)
-      await fs.writeJson(targetPath, sorted, {})
-    } catch (e) {
-      console.log(e)
-      process.exit(1)
-    }
-  })()
+(async () => {
+  try {
+    const dirs = await fs.readdir(pagePrefix);
+    const childDirs = dirs.filter(df => fs.statSync(path.join(pagePrefix, df)).isDirectory());
+    const data = await getMetadata(childDirs, pagePrefix);
+    const sorted = data.sort((a, b) => weights[a.name] - weights[b.name]);
+    const targetPath = getTargetPath();
+    await fs.ensureFile(targetPath);
+    await fs.writeJson(targetPath, sorted, {});
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+})();
