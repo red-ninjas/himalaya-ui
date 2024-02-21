35e4f74584d8aacffcc4e3febe36d79c7658ef99
Ahmad-123 <ahmadnawaz5781@gmail.com>
2024-02-21 18:18:43 +0500

    Moved ConfigProvider to /components/config

--- src/components/use-context/index.ts
+++ src/components/use-context/index.ts
@@ -1,7 +1,4 @@
-import ConfigProvider from './config-provider';
-export type { ConfigProps } from './config-provider';
 export { useConfigs, defaultToastLayout } from './config-context';
 export { default as ThemeProvider } from './theme-provider';
 
 export type { UpdateToastsFunction, UpdateToastsLayoutFunction, UpdateToastsIDFunction } from './config-context';
-export default ConfigProvider;
