6b890f03df09acbd182abcfc72b636b4041eba39
Syed Abubakar <developer@awbnext.com>
2024-02-22 09:33:13 +0500

    add prettier eslint

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   settings: {
     react: {
