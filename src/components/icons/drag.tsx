03d004226f1e60cadde4e531878c25e243e9c386
Syed Abubakar <developer@awbnext.com>
2024-02-23 10:24:03 +0500

    fix(drag-icon): remove hard-coded color

--- src/components/icons/drag.tsx
+++ src/components/icons/drag.tsx
@@ -16,11 +16,7 @@ const Drag = ({ size = 24, color, style, ...props }: IconPropsNative) => {
       width={size}
       style={{ ...style, color: color }}
     >
-      <path
-        fill="#000"
-        stroke="#000"
-        d="M8 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm0-7a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm0 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm8-7a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm0-7a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm0 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
-      />
+      <path d="M8 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm0-7a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm0 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm8-7a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm0-7a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm0 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" />
     </svg>
   );
 };
