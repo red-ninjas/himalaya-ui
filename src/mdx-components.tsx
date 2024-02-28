56afde3f274afa1b6dca53861f525aceceb0f465
Stefan Kalysta <stefan.kalysta@gmail.com>
2024-02-28 13:52:38 +0200

    fix(mdx) Remove test from documentation

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -5,7 +5,7 @@ import { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    a: ({ children, ...props }) => <HybridLink {...(props as LinkProps)}>{children}test</HybridLink>,
+    a: ({ children, ...props }) => <HybridLink {...(props as LinkProps)}>{children}</HybridLink>,
     img: props => <Image {...(props as React.ComponentPropsWithoutRef<'img'>)}></Image>,
     pre: ({ children, ...props }) => <HybridCode {...(props as React.ComponentPropsWithoutRef<'pre'>)}>{children}</HybridCode>,
     ...components,
